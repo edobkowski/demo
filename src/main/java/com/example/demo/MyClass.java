00db7f1eb1d108f43748429df95701572dc76a54
edobkowski <eryk.dobkowski@erif.pl>
2020-01-17 14:18:11 +0100

    dupa8

--- src/main/java/com/example/demo/MyClass.java
+++ src/main/java/com/example/demo/MyClass.java
@@ -6,7 +6,12 @@ import java.util.List;
 
 @CommonsLog
 public final class MyClass {
-    static public String s;
+    private static  String s;
+
+
+    public int add(int a, int b) {
+        return a+b;
+    }
 
     public int foo1() {
         return 1;
@@ -42,11 +47,26 @@ public final class MyClass {
             return 1;
         return 0;
     }
-    private int foo8() {
+    public int foo8() {
         String s = "dupa8";
-        if (s == "dupa8")
+        if (s.equals("dupa8"))
             return 1;
         return 0;
+    }
+
+    private void doNothing() {
+
+    }
+
+    static public String st = null;
+
+    private void method(int i) {
+        i = 1;
+        switch(i) {
+            case 1:
+                System.out.println("dupa8");
+        }
+        while(true);
     }
 
     public void doSomething(String [] strings) {
